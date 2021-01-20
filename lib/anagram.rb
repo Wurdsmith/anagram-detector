cc2863659a4be3da1cb20d1fb3cc006bbf12b0d7
Wurdsmith <wurdsmithing@gmail.com>
2021-01-20 10:53:58 -0600

    commit tree 3b283ab5895e3850b89cc80f0ef26dc1a1ee5e30

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :words
+
+    def initialize(words)
+        @words = words
+    end
+
+    def match(words_list)
+        
+        words_list.select do |word| 
+            @words.split("").sort == word.split("").sort
+        end
+    end
+
+end
